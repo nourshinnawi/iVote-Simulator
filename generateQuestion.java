ec581e5a0afae5fe018a83368cbfa85a36835df4
nnnn <50388570+nourshinnawi@users.noreply.github.com>
2020-10-02 18:58:17 -0700

    Delete generateQuestion.java

--- generateQuestion.java
+++
@@ -1,9 +0,0 @@
-package a1;
-
-public enum generateQuestion {MULTIPLE, SINGLE;
-
-    public static generateQuestion getRandom(){
-            return generateQuestion.values()[(int) 
-                    (Math.random()*generateQuestion.values().length)];
-    }
-};
